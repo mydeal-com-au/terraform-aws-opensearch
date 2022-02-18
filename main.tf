f152f6659a6148f477c6b44ff7e47f9c6eb249b9
Alan dos Santos <adossantos@cochlear.com>
2022-02-18 22:55:42 +1100

    Add master arn user to deploy cluster

--- main.tf
+++ main.tf
@@ -58,9 +58,9 @@ resource "aws_elasticsearch_domain" "opensearch" {
     enabled                        = true
     internal_user_database_enabled = false
 
-    # master_user_options {
-    #   master_user_arn = (var.master_user_arn != "") ? var.master_user_arn : data.aws_caller_identity.current.arn
-    # }
+    master_user_options {
+      master_user_arn = (var.master_user_arn != "") ? var.master_user_arn : data.aws_caller_identity.current.arn
+    }
   }
 
   domain_endpoint_options {

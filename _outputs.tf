4081f51cb94fb77b029a390e21128a0476b64508
matthosking-mydeal <95453715+matthosking-mydeal@users.noreply.github.com>
2024-10-15 09:23:23 +1100

    Update _outputs.tf

--- _outputs.tf
+++ _outputs.tf
@@ -1,11 +1,11 @@
 output "cluster_name" {
   description = "The name of the OpenSearch cluster."
-  value       = aws_elasticsearch_domain.opensearch.domain_name
+  value       = aws_opensearch_domain.opensearch.domain_name
 }
 
 output "cluster_version" {
   description = "The version of the OpenSearch cluster."
-  value       = replace(aws_elasticsearch_domain.opensearch.elasticsearch_version, "OpenSearch_", "")
+  value       = aws_opensearch_domain.opensearch.engine_version
 }
 
 output "cluster_endpoint" {
